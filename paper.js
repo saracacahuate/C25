fd80476673721a8e2c045309ceaef866d658f45d
saracacahuate <90066330+saracacahuate@users.noreply.github.com>
2021-11-20 13:17:10 -0600

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,29 @@
+class paper {
+    constructor(x,y){
+        var options = {
+            isStatic:false,
+            restitution:0.3,
+            friction:0,
+            density:1.2,
+
+        };
+        this.body = Bodies.rectangle(x, y, 50, 50, options);
+        this.width = 50;
+        this.height = 50;
+        this.image = loadImage("paper.png");
+        World.add(world, this.body);
+       };
+       display(){
+           var pos = this.body.position;
+           pos.x = mouseX;
+           pos.y = mouseY;
+           var angle = this.body.angle;
+
+           Push();
+           translate(this.body.position.x, this.body.position.y);
+           rotate(angle);
+           ImageMode(CENTER)
+           image(this.image, 0, 0, this.width, this.height);
+           Pop();
+       }
+}
---
+++ sketch.js
@@ -0,0 +1,38 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+var dustbinObj,groundObject	
+var world;
+
+var paper1;
+
+function setup() {
+	createCanvas(1600, 700);
+	rectMode(CENTER);
+
+
+	engine = Engine.create();
+	world = engine.world;
+	
+	groundObject=new ground(width/2,670,width,20);
+	dustbinObj=new dustbin(1200,650);
+	Engine.run(engine);
+    
+	paper1 = new paper(800,300);
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(230);
+ 
+
+  groundObject.display();
+  dustbinObj.display();
+  paper1.display();
+
+}
+
